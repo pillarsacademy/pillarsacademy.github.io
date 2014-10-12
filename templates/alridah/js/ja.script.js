07ac9b0cc958f6587ccb8ea19650db7470939ec8
Abdullah Jibaly <jibaly@fb.com>
2014-10-12 00:35:53 -0700

    Add local media, css, and images

---
+++ templates/alridah/js/ja.script.js
@@ -0,0 +1,178 @@
+var siteurl = '';
+
+function fixIEPNG(el, bgimgdf, sizingMethod, type, offset){
+	var objs = el;
+	if(!objs) return;
+	if ($type(objs) != 'array') objs = [objs];
+	if(!sizingMethod) sizingMethod = 'crop';
+	if(!offset) offset = 0;
+	var blankimg = siteurl + 'images/blank.png';
+	objs.each(function(obj) {
+		var bgimg = bgimgdf;
+		if (obj.tagName == 'IMG') {
+			//This is an image
+			if (!bgimg) bgimg = obj.src;
+			if (!(/\.png$/i).test(bgimg) || (/blank\.png$/i).test(bgimg)) return;
+
+			obj.setStyle('height',obj.offsetHeight);
+			obj.setStyle('width',obj.offsetWidth);
+			obj.src = blankimg;
+			obj.setStyle ('visibility', 'visible');
+			obj.setStyle('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="+bgimg+", sizingMethod='"+sizingMethod+"')");
+		}else{
+			//Background
+			if (!bgimg) bgimg = obj.getStyle('backgroundImage');
+			var pattern = new RegExp('url\s*[\(\"\']*([^\'\"\)]*)[\'\"\)]*');
+			if ((m = pattern.exec(bgimg))) bgimg = m[1];
+			if (!(/\.png$/i).test(bgimg) || (/blank\.png$/i).test(bgimg)) return;
+			if (!type)
+			{
+				obj.setStyle('background', 'none');
+				//if(!obj.getStyle('position'))
+				if(obj.getStyle('position')!='absolute' && obj.getStyle('position')!='relative') {
+					obj.setStyle('position', 'relative');
+				}
+
+				//Get all child
+				var childnodes = obj.childNodes;
+				for(var j=0;j<childnodes.length;j++){
+					if((child = $(childnodes[j]))) {
+						if(child.getStyle('position')!='absolute' && child.getStyle('position')!='relative') {
+							child.setStyle('position', 'relative');
+						}
+						child.setStyle('z-index',2);
+					}
+				}
+				//Create background layer:
+				var bgdiv = new Element('IMG');
+				bgdiv.src = blankimg;
+				bgdiv.width = obj.offsetWidth - offset;
+				bgdiv.height = obj.offsetHeight - offset;
+				bgdiv.setStyles({
+					'position': 'absolute',
+					'top': 0,
+					'left': 0
+				});
+
+				bgdiv.className = 'TransBG';
+
+				bgdiv.setStyle('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="+bgimg+", sizingMethod='"+sizingMethod+"')");
+				bgdiv.inject(obj, 'top');
+				//alert(obj.innerHTML + '\n' + bgdiv.innerHTML);
+			} else {
+				obj.setStyle('filter', "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="+bgimg+", sizingMethod='"+sizingMethod+"')");
+			}
+		}
+	}.bind(this));
+}
+
+function switchFontSize (ckname,val){
+	var bd = $E('body');
+	switch (val) {
+		case 'inc':
+		if (CurrentFontSize+1 < 7) {
+			bd.removeClass('fs'+CurrentFontSize);
+			CurrentFontSize++;
+			bd.addClass('fs'+CurrentFontSize);
+		}
+		break;
+		case 'dec':
+		if (CurrentFontSize-1 > 0) {
+			bd.removeClass('fs'+CurrentFontSize);
+			CurrentFontSize--;
+			bd.addClass('fs'+CurrentFontSize);
+		}
+		break;
+		default:
+		bd.removeClass('fs'+CurrentFontSize);
+		CurrentFontSize = val;
+		bd.addClass('fs'+CurrentFontSize);
+	}
+	Cookie.set(ckname, CurrentFontSize,{duration:365});
+}
+
+function switchTool (ckname, val) {
+	createCookie(ckname, val, 365);
+	window.location.reload();
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+//String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ""); };
+
+//addEvent - attach a function to an event
+function jaAddEvent(obj, evType, fn){
+	if (obj.addEventListener){
+		obj.addEventListener(evType, fn, false);
+		return true;
+	} else if (obj.attachEvent){
+		var r = obj.attachEvent("on"+evType, fn);
+		return r;
+	} else {
+		return false;
+	}
+}
+
+function equalHeight(){	
+	//
+	var ja_col = $('ja-col');
+	var ja_mainbody = $('ja-mainbody');
+	offset = 0;
+	if (ja_mainbody.offsetHeight < ja_col.offsetHeight) {
+		var offset = ja_col.offsetHeight - ja_mainbody.offsetHeight;
+	}
+	if ($('ja-botsl')) {
+		makeEqualHeight ($('ja-botsl').getChildren(),offset);
+		offset = 0;
+	}
+	makeEqualHeight ($$('.jazin-box'), offset);
+	
+}
+
+function makeEqualHeight(divs, offset) {
+	if (!offset) offset = 0;
+	if(!divs || divs.length < 2) return;
+	var maxh = 0;
+	divs.each(function(el, i){
+		var ch = el.getCoordinates().height;
+		maxh = (maxh < ch) ? ch : maxh;
+	},this);
+	maxh += offset;
+	divs.each(function(el, i){
+		el.setStyle('height', maxh-el.getStyle('padding-top').toInt()-el.getStyle('padding-bottom').toInt());
+	},this);
+}
+
+function getDeepestDiv (div) {
+	while (div.getChildren().length==1 && (div.getChildren()[0].tagName == 'DIV'))
+	{
+		div = div.getChildren()[0];
+	}
+	return div;
+}
+
+function preloadImages () {
+	var imgs = new Array();
+	for (var i = 0; i < arguments.length; i++) {
+		var imgsrc = arguments[i];
+		imgs[i] = new Image();
+		imgs[i].src = imgsrc;
+	}
+}
+
+function isIE6() {
+	version=0
+	if (navigator.appVersion.indexOf("MSIE")!=-1){
+		temp=navigator.appVersion.split("MSIE")
+		version=parseFloat(temp[1])
+	}
+	return (version && (version < 7));
+}
